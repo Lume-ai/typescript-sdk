71432644ec31f2833f70761e27ad4ecf0cad66a3
Nicolas Machado <nicolas@lume.ai>
2024-04-25 16:44:24 -0400

    Removing fs import module

--- src/utils/helperService/fileReader/NodeFileReader.ts
+++
@@ -1,9 +0,0 @@
-import { promises as fs } from 'fs';
-import { FileReaderInterface } from './FileReaderInterface';
-
-export class NodeFileReader implements FileReaderInterface {
-    async readAsBinaryString(filePath: string): Promise<string> {
-        const buffer = await fs.readFile(filePath);
-        return buffer.toString('binary');
-    }
-}
--- src/utils/helperService/fileReader/getFileReader.ts
+++ src/utils/helperService/fileReader/getFileReader.ts
@@ -1,12 +1,9 @@
 import { FileReaderInterface } from "./FileReaderInterface";
-import { NodeFileReader } from "./NodeFileReader";
 import { WebFileReader } from "./WebFileReader";
 
 export function getFileReader(): FileReaderInterface {
     if (typeof window !== 'undefined' && typeof FileReader !== 'undefined') {
         return new WebFileReader();
-    } else if (typeof process !== 'undefined') {
-        return new NodeFileReader();
     } else {
         throw new Error('Unsupported environment');
     }
